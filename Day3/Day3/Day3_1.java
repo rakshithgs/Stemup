c40a79b395dabc3360448c4f043338dc89ab086f
rakshithgs <gsrakshith777@gmail.com>
2025-07-02 15:46:48 +0530

    Create Day3_1.java

---
+++ Day3/Day3/Day3_1.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Day3_1 {
+   public static void main(String[] args) {
+   for(int i=0;i<1000;i++){
+    System.out.println("Hello!");
+   }
+    
+    }
+  }
+    
+    
+
