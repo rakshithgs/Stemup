88b806b055f921e5241550e29dd1ada37dd94335
rakshithgs <gsrakshith777@gmail.com>
2025-07-02 22:10:37 +0530

    .

--- Day3/Day3/Day3_6.java
+++ Day3/Day3/Day3_6.java
@@ -1,10 +1,13 @@
 package Day3;
 
-public class day3_6 {
+public class Day3_6 {
+    public static void main(String[] args) {
+    
     int counter = 0;
 while (counter < 5) {
   System.out.println("Hello");
   counter++; // increment counter
 }
 
+}
 }
